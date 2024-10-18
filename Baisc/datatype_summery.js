2710cd2cb167799041b0d2ac38df48e840483555
Ketan9548 <kc918062@gmail.com>
2024-10-18 09:32:57 +0530

    js practies

---
+++ Baisc/datatype_summery.js
@@ -0,0 +1,14 @@
+// primative 
+
+// 7 type = String , Number , Boolean , null, undefined, Symbol
+
+// Js is Dynamic Type Language , not declayer the data type in any value
+
+// refrence type (Non Primative )
+// Array , Objects , Functinon
+
+
+let binumber = 1234221313131412121213133213124143423232n
+
+console.log(binumber);
+
