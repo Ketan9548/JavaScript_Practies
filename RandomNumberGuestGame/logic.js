ef94cb486ae7650d22adfea6a4c6c7e0847a50a8
Ketan9548 <kc918062@gmail.com>
2024-12-16 12:10:48 +0530

    done

--- RandomNumberGuestGame/logic.js
+++ RandomNumberGuestGame/logic.js
@@ -1,14 +1,19 @@
 let val = document.getElementById("number");
 const inputelement = document.getElementById("inputval");
+const text = document.getElementById("text");
+let count = 0;
 
 function random() {
-  let inputval = parseInt(inputelement.value, 10); // Convert input to number
-  let randomnumber = Math.floor(Math.random() * 5); // Random number between 0-4
-  val.innerHTML = randomnumber.toString(); // Display the random number in the "number" element
-
-  if (inputval === randomnumber) {
+  let inputval = parseInt(inputelement.value, 10);
+  let randomnumber = Math.floor(Math.random() * 100);
+  val.innerHTML = randomnumber.toString();
+  count++;
+  if (inputval === randomnumber && count === 1) {
     alert("Won the game");
+  } else if (count === 10) {
+    inputelement.value = "";
+    alert("you exceeded the limit");
   } else {
-    alert("Try again!");
+    text.innerHTML = "Try again";
   }
 }
